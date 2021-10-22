0073289e68171cbca4c87558dbe17cf5b3cf5480
RamahAcnt <ramahravi11@gmail.com>
2021-10-22 21:18:29 +0530

    SampleProgram

---
+++ src/main/java/org/samp/Employee.java
@@ -0,0 +1,22 @@
+package org.samp;
+
+public class Employee {
+	
+	public void name() {
+		
+		System.out.println("My name is Ramah");
+
+	}
+	public void mobile() {
+System.out.println("My mobile number is 7200510608");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e=new Employee();
+		e.name();
+		e.mobile();
+				
+	}
+
+}
